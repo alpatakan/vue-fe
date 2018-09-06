5ef5bbbec2ed9bb222909ff10b3e90b6d2418007
Alp Atakan Isbakan <alpatakan92@gmail.com>
2018-09-06 23:02:26 +0300

    last commit before funeral

--- src/components/Pdr/PdrMain.vue
+++ src/components/Pdr/PdrMain.vue
@@ -1,22 +1,44 @@
 <template>
-  <div class="pdrMain">
-    <h1>{{title}}</h1>
+  <div class="container">
+    <div id="app">
+      <tabs>
+        <tab name="Tab One" :selected="true">
+          <h1>Composer is great</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus quae repellendus vel! Accusamus aliquid,
+            consequuntur culpa cumque esse ex fugiat impedit inventore laudantium maiores molestiae officia provident quod
+            totam veniam!</p>
+        </tab>
+        <tab name="Tab Taylor Otwell">
+          <h1>Vagrant is awesome</h1>
+          <p>A animi, delectus deleniti dignissimos dolorum ea eveniet hic, impedit, magnam odio tempore ut. Assumenda
+            blanditiis dolorem doloremque eos, id magni maxime necessitatibus odio porro ratione repudiandae tempore
+            voluptate voluptatibus.</p>
+        </tab>
+        <tab name="Laravel and Vuejs">
+          <h1>Docker is way better</h1>
+          <p>Animi fuga in inventore quibusdam sed sint. Ab alias, aliquid at aut commodi culpa ea eligendi ex ipsam
+            libero minus nam nobis numquam obcaecati placeat praesentium quis quod tempore voluptatum?</p>
+        </tab>
+        <tab name="Optimus Prime">
+          <h1>What is Node.js</h1>
+          <p>Accusamus cupiditate dolor doloribus illo ipsam itaque laborum, maxime natus numquam odit officiis possimus
+            quidem voluptatem! Atque dignissimos labore modi nobis quasi quo sit! Aspernatur ea perspiciatis repellendus
+            reprehenderit. Laboriosam?</p>
+        </tab>
+      </tabs>
+    </div>
   </div>
 </template>
-<script>
-  export default {
-    name: 'pdrMain',
-    data () {
-      return {
-        title: 'dummy main page',
 
-      }
-    },
-    methods: {
+<script>
+  import Tabs from 'src/components/Pdr/tabs.vue'
+  import Tab from 'src/components/Pdr/tab.vue'
+
+  export default {
+    components: {
+      Tabs,
+      Tab
     }
   }
 
 </script>
-<style scoped>
-
-</style>
