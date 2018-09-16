a3b9a90291e40cde83b157544ed4ea1e19baf0ac
Alp Atakan Isbakan <alpatakan92@gmail.com>
2018-09-16 18:49:07 +0300

    add main page template with navbar

---
+++ pedereselamsoyle/src/components/PdrMain.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div id="navbar-container">
+      <tabs>
+        <tab name="Tab One" :selected="true">
+          <h1>Composer is great</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus quae repellendus vel! Accusamus aliquid,
+            consequuntur culpa cumque esse ex fugiat impedit inventore laudantium maiores molestiae officia provident quod
+            totam veniam!</p>
+        </tab>
+        <tab name="Tab Taylor Otwell">
+          <h1>Vagrant is awesome</h1>
+          <p>A animi, delectus deleniti dignissimos dolorum ea eveniet hic, impedit, magnam odio tempore ut. Assumenda
+            blanditiis dolorem doloremque eos, id magni maxime necessitatibus odio porro ratione repudiandae tempore
+            voluptate voluptatibus.</p>
+        </tab>
+        <tab name="Laravel and Vuejs">
+          <h1>Docker is way better</h1>
+          <p>Animi fuga in inventore quibusdam sed sint. Ab alias, aliquid at aut commodi culpa ea eligendi ex ipsam
+            libero minus nam nobis numquam obcaecati placeat praesentium quis quod tempore voluptatum?</p>
+        </tab>
+        <tab name="Optimus Prime">
+          <h1>What is Node.js</h1>
+          <p>Accusamus cupiditate dolor doloribus illo ipsam itaque laborum, maxime natus numquam odit officiis possimus
+            quidem voluptatem! Atque dignissimos labore modi nobis quasi quo sit! Aspernatur ea perspiciatis repellendus
+            reprehenderit. Laboriosam?</p>
+        </tab>
+      </tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Tabs from '@/components/navigation-bar/Tabs'
+  import Tab from '@/components/navigation-bar/Tab'
+
+  export default {
+    components: {
+      Tabs,
+      Tab
+    }
+  }
+
+</script>
---
+++ pedereselamsoyle/src/components/navigation-bar/Tabs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="applicationContainer">
+    <div id="navBarContainer" class="navbar">
+      <ul class="nav">
+        <li class="nav-item" :class="{ active: menuItem.isActive }" v-for="menuItem in navItemsArray"><a :href="menuItem.href" @click="selectTabItem(menuItem)">{{ menuItem.name }}</a></li>
+      </ul>
+    </div>
+
+    <div id="tabContentContainer">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        navItemsArray: []
+      }
+    },
+    mounted() {
+      this.navItemsArray = this.$children
+    },
+    methods: {
+      selectTabItem(passedAlongMenuItemObject) {
+        this.navItemsArray.forEach(navItem => {
+          navItem.isActive = (passedAlongMenuItemObject == navItem)
+        })
+      }
+    }
+  }
+</script>
