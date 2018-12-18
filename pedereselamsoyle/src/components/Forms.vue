a68988ecb5a6cb4cdd0b0497f96ac847f230a8d5
Alp Atakan Isbakan <alpatakan92@gmail.com>
2018-12-18 21:13:29 +0300

    forms update

--- pedereselamsoyle/src/components/Forms.vue
+++ pedereselamsoyle/src/components/Forms.vue
@@ -4,12 +4,12 @@
 		<!-- Main table element -->
 			<b-table show-empty
 					 ref="table"
-					 :items="formMetaList"
+					 :items="formList"
 					 :fields="fields"
 			>
 			<template slot="actions" slot-scope="row">
 				<!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
-				<b-button size="sm" @click.stop="row.toggleDetails" @click="loadTest(row.item)" class="mr-2">
+				<b-button size="sm" @click.stop="loadTest(row)" class="mr-2">
 					{{ row.detailsShowing ? 'Hide' : 'Show'}} Details
 				</b-button>
 				<b-button size="sm" v-on:click.stop="removeTest(row.item)" class="mr-1">
@@ -18,9 +18,17 @@
 			</template>
 			<template slot="row-details" slot-scope="row">
 				<b-card>
-					<b-row class="mb-2">
-						<img v-show="!row.item.loaded" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-					</b-row>
+					<img v-show="!row.item.loaded" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+						<!-- Radio Form -->
+					<b-form-group v-for="(question, index) in row.item.questions" :key="index" :label="question.metadata.number + ': ' + question.text">
+						<div v-if="question.metadata.type == 'RADIO'">
+							<b-form-radio-group :id="'btnradios' + question.metadata.number" buttons :options="question.options">
+							</b-form-radio-group>
+						</div>
+						<div v-if="question.metadata.type == 'TEXT'">
+						</div>
+					</b-form-group>
+					<b-button v-show="row.item.loaded" size="sm" @click="row.toggleDetails">Hide Details</b-button>
 				</b-card>
 			</template>
 		</b-table>
@@ -32,12 +40,13 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import axios from "axios";
 
 export default {
 	data() {
 		return {
-			formMetaList: [],
+			formList: [],
 			instance: {},
 			fields: [
 				{
@@ -71,8 +80,7 @@ export default {
 		this.instance
 			.get("/forms/")
 			.then(response => {
-				console.log(response.data.data);
-				this.formMetaList = response.data.data;
+				this.formList = response.data.data;
 			})
 			.catch(error => {
 				console.log(error);
@@ -85,25 +93,43 @@ export default {
 				.delete("/forms/" + testItem._id)
 				.then(response => {
 					console.log(response);
-					var index = this.formMetaList.indexOf(testItem);
+					var index = this.formList.indexOf(testItem);
 
 					if (index !== -1) {
-						this.formMetaList.splice(index, 1);
+						this.formList.splice(index, 1);
 					}
 				})
 				.catch(error => {
 					console.log(error);
 				});
 		},
-		loadTest(testItem) {
-			console.log("load testId = " + testItem._id);
+		loadTest(row) {
+			var testItem = row.item;
+
+			row.toggleDetails();
+			if (!testItem.loaded) {
+				console.log("load testId = " + testItem._id);
+				this.instance
+					.get("/forms/" + testItem._id)
+					.then(response => {
+						var index = this.formList.indexOf(testItem);
+
+						this.formList[index]["questions"] = response.data.data.questions;
+						console.log("questions");
+						console.log(this.formList[index]);
+						Vue.set(testItem, 'loaded', 1);
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			};
 		},
 		renewForms() {
 			this.instance
 				.patch("/forms/")
 				.then(response => {
 					console.log(response.data.data);
-					this.formMetaList.push(response.data.data);
+					this.formList.push(response.data.data);
 				})
 				.catch(error => {
 					console.log(error);
