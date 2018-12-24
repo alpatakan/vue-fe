b8cbaaa5e5452e6e31080a6897061453ba27642c
Alp Atakan Isbakan <alpatakan92@gmail.com>
2018-12-25 00:42:57 +0300

    add missing file

---
+++ pedereselamsoyle/src/components/Popform.vue
@@ -0,0 +1,33 @@
+<template>
+	<modal :name="name" :scrollable=true :height="'auto'">
+		<b-container>
+			<slot/>
+		</b-container>
+	</modal>
+</template>
+
+<script>
+import Vue from 'vue'
+import VModal from 'vue-js-modal'
+ 
+Vue.use(VModal)
+
+export default {
+	props: {
+		name: { required: true },
+	},
+	mounted() {
+		console.log('mount edildi popform');
+	},
+	methods: {
+		show (id) {
+			console.log(id + " show modal fk")
+			this.$modal.show(id);
+		},
+		hide (id) {
+			console.log(id + " hide modal fk")
+			this.$modal.hide(id);
+		}
+	}
+}
+</script>
