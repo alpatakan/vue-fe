5dbc5c57d07b87f79aada7b7d1dd6fcbb03ef7c0
Alp Atakan Isbakan <alpatakan92@gmail.com>
2019-01-30 00:32:55 +0300

    add initial editform pages

---
+++ pedereselamsoyle/src/components/EditForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        // form builder template
+        <form-builder type="template" v-model="formData"></form-builder>
+		<b-button size="sm" :to="{ name: 'editGui', query: { formData }}">
+			Edit Gui
+		</b-button>
+		<b-button size="sm" @click="formSave()">
+			console log
+		</b-button>
+    </div>
+</template>
+
+<script>
+import FormBuilder from 'v-form-builder';
+
+export default {
+	components: {
+		FormBuilder
+	},
+	mounted() {
+		console.log('mount edildi edit');
+	},
+	data() {
+		return {
+			formData:{},
+		}
+	},
+	methods: {
+		formSave() {
+			console.log(JSON.stringify(this.formData))
+		}
+	},
+}
+</script>
---
+++ pedereselamsoyle/src/components/EditGui.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <form-builder type="gui" :form="formData"></form-builder>
+		<b-button size="sm" @click="formSave()">
+		</b-button>
+    </div>
+</template>
+
+<script>
+import FormBuilder from 'v-form-builder';
+
+export default {
+	components: {
+		FormBuilder
+	},
+	created() {
+		this.debFormBuilderug = this.$route.query;
+	},
+	mounted() {
+		console.log('mount edildi editGui');
+	},
+	data() {
+		return {
+			formData:{},
+		}
+	},
+	methods: {
+		formSave() {
+			console.log(this.formData)
+		}
+	},
+}
+</script>
